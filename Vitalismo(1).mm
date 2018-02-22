266f099f2beaf8344ddfe2be415046946e236d33
Marc Bundler <marc@liberada.net>
2018-02-22 16:22:07 +0100

    Add files via upload

---
+++ Vitalismo(1).mm
@@ -0,0 +1,243 @@
+<map version="1.0.1">
+    <node ID="ID_1" LINK="http://vitalismo.org/principios" TEXT="&lt;-- Comunidad --Vitalismo-- Axiomas --&gt;">
+        <node ID="ID_12" POSITION="right" STYLE="fork" TEXT="Conectividad">
+            <node ID="ID_13" POSITION="right" STYLE="fork">
+                <richcontent TYPE="NODE">
+                    <html>
+                        <head/>
+                        <body>
+                            <p>Toda materia o informaci&#243;n esta viva como nosotras, aunque no tenga tan complejos organismos, nervios o sentimientos</p>
+                        </body>
+                    </html>
+                </richcontent>
+            </node>
+            <node ID="ID_10" POSITION="right" STYLE="fork" TEXT="Plenitud">
+                <node ID="ID_11" POSITION="right" STYLE="fork" TEXT="Todo est&#225; lleno. El vacio o el infinito es un efecto percepcional. Mejor decir Vacio posicional que vacio existencial "/>
+            </node>
+            <node ID="ID_21" POSITION="right" STYLE="fork" TEXT="Equilibrio">
+                <node ID="ID_20" POSITION="right" STYLE="fork" TEXT="Hay un total de energ&#237;a tensionada que ni se crea ni se destruye, se transforma movi&#233;ndose entre polos"/>
+                <node ID="ID_53" POSITION="right" STYLE="fork" TEXT="Amorosidad">
+                    <node ID="ID_55" POSITION="right" STYLE="fork" TEXT="El amor es una mejor palabra que monismo, fuerza vital, movimiento, inercia o la entropia como fuente (y no como resto)"/>
+                    <node ID="ID_56" POSITION="right" STYLE="fork" TEXT="Reductividad">
+                        <node ID="ID_54" POSITION="right" STYLE="fork" TEXT="Nada puede desear ni dejar de amar a lo que ama que amemos, tampoco nada puede amar a lo que no le ama "/>
+                    </node>
+                </node>
+            </node>
+            <node ID="ID_2" POSITION="right" STYLE="fork" TEXT="Unidad">
+                <node ID="ID_52" POSITION="right" STYLE="fork" TEXT="Una supuesta separaci&#243;n permite la simulaci&#243;n de una experiencia con una determinada agencia"/>
+                <node ID="ID_61" POSITION="right" STYLE="fork" TEXT="Mejorabilidad">
+                    <node ID="ID_62" POSITION="right" STYLE="fork" TEXT="Esta galaxia es un laboratorio estable de un multiverso que se nos quiere llegar a mostrar como un ohmniverso supersim&#233;trico"/>
+                </node>
+                <node ID="ID_51" POSITION="right" STYLE="fork" TEXT="Entrelazamiento">
+                    <node ID="ID_28" POSITION="right" STYLE="fork" TEXT="Ning&#250;n fen&#243;meno esta aislado del resto de eventos, la observaci&#243;n somos todas interactuando en grupo"/>
+                    <node ID="ID_39" POSITION="right" STYLE="fork" TEXT="Escalabilidad">
+                        <node ID="ID_34" POSITION="right" STYLE="fork" TEXT="&#193;tomos, planetas, conceptos, ideas funcionan con la misma mec&#225;nica escalada a diferentes proyecciones"/>
+                        <node ID="ID_35" POSITION="right" STYLE="fork" TEXT="Integralidad">
+                            <node ID="ID_38" POSITION="right" STYLE="fork" TEXT="En todas las interacciones de la materia se siguen manifiestando los mismos principios fundamentales"/>
+                        </node>
+                    </node>
+                </node>
+                <node ID="ID_32" POSITION="right" STYLE="fork" TEXT="Reciprocidad">
+                    <node ID="ID_33" POSITION="right" STYLE="fork" TEXT="Acelerar un hadr&#243;n tira de la malla, lo ocultado tambi&#233;n vibra, nada escapa a la ley del karma "/>
+                </node>
+                <node ID="ID_41" POSITION="right" STYLE="fork" TEXT="Complementariedad">
+                    <node ID="ID_40" POSITION="right" STYLE="fork" TEXT="Yin Yangueado(+) y viceversa son dos constantes polarizadas tambi&#233;n percibidas con dualidad (Yang-) percepcional difusa"/>
+                    <node ID="ID_60" POSITION="right" STYLE="fork" TEXT="Accesibilidad">
+                        <node ID="ID_59" POSITION="right" STYLE="fork" TEXT="El Yang- es falta de Yang+ o de Yin, y sobretodo es falta de YinYang+"/>
+                    </node>
+                </node>
+                <node ID="ID_25" POSITION="right" STYLE="fork" TEXT="Difusividad">
+                    <node ID="ID_24" POSITION="right" STYLE="fork" TEXT="Lo &quot;&#250;nico&quot; o &quot;diferente&quot; es una percepci&#243;n de una diferencia de cantidad de alguna de sus polaridades"/>
+                </node>
+            </node>
+        </node>
+        <node ID="ID_79" POSITION="left" STYLE="fork" TEXT="Collaborativity">
+            <node ID="ID_81" POSITION="left" STYLE="fork">
+                <richcontent TYPE="NODE">
+                    <html>
+                        <head/>
+                        <body>
+                            <p>We want to share friendly vibes within us, and with whoever reads us. Despite research being a competitive race by essence, a cooperative attitude is what guide us as a whole.</p>
+                        </body>
+                    </html>
+                </richcontent>
+            </node>
+        </node>
+        <node ID="ID_82" POSITION="left" STYLE="fork" TEXT="Free culture">
+            <node ID="ID_80" POSITION="left" STYLE="fork">
+                <richcontent TYPE="NODE">
+                    <html>
+                        <head/>
+                        <body>
+                            <p>All our promoted and produced contents will be released with open standards, with the public domain and-or with a free culture and-or free software licence conditions.</p>
+                            <p/>
+                        </body>
+                    </html>
+                </richcontent>
+            </node>
+        </node>
+        <node ID="ID_86" POSITION="left" STYLE="fork" TEXT="Rigourosity">
+            <node ID="ID_85" POSITION="left" STYLE="fork">
+                <richcontent TYPE="NODE">
+                    <html>
+                        <head/>
+                        <body>
+                            <p>Stands shouldn't have been already falsified, and they should always be open to be reviewed so key questions made to them have to be replied and criticisms documented</p>
+                        </body>
+                    </html>
+                </richcontent>
+            </node>
+        </node>
+        <node ID="ID_84" POSITION="left" STYLE="fork" TEXT="Simplicity">
+            <node ID="ID_83" POSITION="left" STYLE="fork">
+                <richcontent TYPE="NODE">
+                    <html>
+                        <head/>
+                        <body>
+                            <p>Our phrasings have to  be the more easily understood by the general public. More complex quotes reaffirming the stands are to be welcomed</p>
+                        </body>
+                    </html>
+                </richcontent>
+            </node>
+        </node>
+        <node ID="ID_89" POSITION="left" STYLE="fork" TEXT="Beautiful">
+            <node ID="ID_87" POSITION="left" STYLE="fork">
+                <richcontent TYPE="NODE">
+                    <html>
+                        <head/>
+                        <body>
+                            <p>Expressions should be open to be tunned with whatever other expressive mean, i.e. transmedia, multisensorial and poethical aesthetics are very welcomed</p>
+                        </body>
+                    </html>
+                </richcontent>
+            </node>
+        </node>
+        <node ID="ID_91" POSITION="left" STYLE="fork" TEXT="Crowdsourcing">
+            <node ID="ID_90" POSITION="left" STYLE="fork">
+                <richcontent TYPE="NODE">
+                    <html>
+                        <head/>
+                        <body>
+                            <p>Our productions should be open to be enriched the most peer-to-peer posible, we should develop strategic tools that could help theorists and researchers in this way.</p>
+                        </body>
+                    </html>
+                </richcontent>
+            </node>
+        </node>
+        <node ID="ID_88" POSITION="left" STYLE="fork" TEXT="Decentralization">
+            <node ID="ID_92" POSITION="left" STYLE="fork">
+                <richcontent TYPE="NODE">
+                    <html>
+                        <head/>
+                        <body>
+                            <p>While we shouldn't corporate all practical tools we individually want to develop, at least we should leave open for debate all practical ways for our community to thrive.</p>
+                        </body>
+                    </html>
+                </richcontent>
+            </node>
+        </node>
+        <node ID="ID_93" POSITION="left" STYLE="fork" TEXT="Aggregating">
+            <node ID="ID_94" POSITION="left" STYLE="fork">
+                <richcontent TYPE="NODE">
+                    <html>
+                        <head/>
+                        <body>
+                            <p>We should develop aggregators with publications of researchers, and update or fork inventories of related resources that pursue our aims</p>
+                        </body>
+                    </html>
+                </richcontent>
+            </node>
+        </node>
+        <node ID="ID_96" POSITION="left" STYLE="fork" TEXT="Ambition">
+            <node ID="ID_95" POSITION="left" STYLE="fork">
+                <richcontent TYPE="NODE">
+                    <html>
+                        <head/>
+                        <body>
+                            <p>We want this community to thrive and have the highest positive impact in society. We could have our own confort zones and affinity groups, but this community should be pursue</p>
+                            <p>constant updating and membership infinitiness</p>
+                        </body>
+                    </html>
+                </richcontent>
+            </node>
+        </node>
+        <node ID="ID_97" POSITION="left" STYLE="fork" TEXT="Evolution">
+            <node ID="ID_98" POSITION="left" STYLE="fork">
+                <richcontent TYPE="NODE">
+                    <html>
+                        <head/>
+                        <body>
+                            <p>While we see vitalist as a holistic theory, we can't expect in the short term to specify an integrality of practices within a natural law of universal categories,</p>
+                            <p>although we should help those developments in some way, yet to be further debated and specified, since they are our more thrilling challenge that as brave community we should not refuse to face.</p>
+                        </body>
+                    </html>
+                </richcontent>
+            </node>
+        </node>
+        <node ID="ID_27" POSITION="right" STYLE="fork" TEXT="Intencionalidad">
+            <node ID="ID_26" POSITION="right" STYLE="fork" TEXT="Hay una fina configuraci&#243;n causal, para poder vivenciar esta realidad desde esta simulada libertad"/>
+            <node ID="ID_31" POSITION="right" STYLE="fork" TEXT="Explicabilidad">
+                <node ID="ID_30" POSITION="right" STYLE="fork" TEXT="La mas evolucionada complejidad se revela desde las mas simples realidades, por ejemplo t&#237;tulos, diadas y triadas"/>
+                <node ID="ID_71" POSITION="right" STYLE="fork" TEXT="Naturalidad">
+                    <node ID="ID_72" POSITION="right" STYLE="fork" TEXT="Hay unas categor&#237;as universales simplificables que se pueden mecanizar en pr&#225;cticas concretas"/>
+                    <node ID="ID_73" POSITION="right" STYLE="fork" TEXT="Gramaticalidad">
+                        <node ID="ID_74" POSITION="right" STYLE="fork" TEXT="La f&#237;sica se puede axiomatizar (Hilbert 6 resuelto), hay una gram&#225;tica universal (o lenguaje del amor) "/>
+                    </node>
+                </node>
+                <node ID="ID_36" POSITION="right" STYLE="fork" TEXT="Parcialidad">
+                    <node ID="ID_37" POSITION="right" STYLE="fork" TEXT=" Materia es informaci&#243;n procesable. Antimateria, materia oscura y energia oscura nos es vida desconocida o inconocible "/>
+                </node>
+                <node ID="ID_67" POSITION="right" STYLE="fork" TEXT="Palabrujas!">
+                    <node ID="ID_69" POSITION="right" STYLE="fork" TEXT="Interactus es el HomoSapiens+ autoconsciente de que est&#233; en un entorno de interacciones f&#237;sicas unificadas"/>
+                </node>
+            </node>
+            <node ID="ID_23" POSITION="right" STYLE="fork" TEXT="Biocentricidad">
+                <node ID="ID_64" POSITION="right" STYLE="fork" TEXT="Lo mas complejo material le es mas dir&#233;ctamente rec&#237;proco a lo material mas simple y a lo mas complejo inmaterial "/>
+                <node ID="ID_65" POSITION="right" STYLE="fork" TEXT="Honorabilidad">
+                    <node ID="ID_22" POSITION="right" STYLE="fork" TEXT="La complejizaci&#243;n molecular (re)genera la gaiaxia (o suelo gal&#225;ctico) y la utersi&#243;n (o tensi&#243;n universal) "/>
+                </node>
+                <node ID="ID_18" POSITION="right" STYLE="fork" TEXT="Humildad">
+                    <node ID="ID_19" POSITION="right" STYLE="fork" TEXT="Aporias inconocibles nos guian, evolucionamos r&#225;pido por ser qui&#233;n se imagina con menos miedos (=+Amor Verdadero) a sus desconocidos"/>
+                </node>
+                <node ID="ID_42" POSITION="right" STYLE="fork" TEXT="Evoluci&#243;n">
+                    <node ID="ID_46" POSITION="right" STYLE="fork" TEXT="Continuidad">
+                        <node ID="ID_48" POSITION="right" STYLE="fork" TEXT="Nuestra vida es una continuaci&#243;n de una intenci&#243;n armoniosa que va evolucionando y complejiz&#225;ndose"/>
+                    </node>
+                    <node ID="ID_43" POSITION="right" STYLE="fork" TEXT="Raz&#243;nabilidad">
+                        <node ID="ID_47" POSITION="right" STYLE="fork" TEXT="La materia mas compleja evoluciona a la necesidad de razonar para la compartici&#243;n de datos revelados"/>
+                        <node ID="ID_63" POSITION="right" STYLE="fork" TEXT="Colaboratividad">
+                            <node ID="ID_66" POSITION="right" STYLE="fork" TEXT="Somos los organismos mas capaces de unificar nuestra interactividad para aumentar la diversidad percepcional"/>
+                        </node>
+                        <node ID="ID_68" POSITION="right" STYLE="fork" TEXT="Poetividad">
+                            <node ID="ID_70" POSITION="right" STYLE="fork" TEXT="La raz&#243;n es una tecnificaci&#243;n de la poesia, aumenta porqu&#233; sus analog&#237;as lo conectan todo con mucha mas facilidad "/>
+                        </node>
+                    </node>
+                    <node ID="ID_49" POSITION="right" STYLE="fork" TEXT="Finalidad">
+                        <node ID="ID_50" POSITION="right" STYLE="fork" TEXT="Interactuamos por amor para superar las interferencias causadas por los temores vibrados en nuestros organismos limitados"/>
+                    </node>
+                </node>
+                <node ID="ID_5" POSITION="right" STYLE="fork" TEXT="Noeticidad">
+                    <node ID="ID_15" POSITION="right" STYLE="fork" TEXT="Las fenomenologias se explican desde nuestro vivir, la fuente es el alma de Gaia. Singularidad es una expresi&#243;n Yangueada de ella"/>
+                    <node ID="ID_3" POSITION="right" STYLE="fork" TEXT="Generolog&#237;a">
+                        <node ID="ID_7" POSITION="right" STYLE="fork" TEXT="La historia se explica con la convivencia de energ&#237;as femeninas y masculinas en organismos y sustancias"/>
+                        <node ID="ID_14" POSITION="right" STYLE="fork" TEXT="+Feminidad">
+                            <node ID="ID_8" POSITION="right" STYLE="fork" TEXT="Tao, Todo o Queer es primario. Yin, Vida, Femenino es segundo. Yang, Mundo, Masculino es tercero"/>
+                            <node ID="ID_44" POSITION="right" STYLE="fork" TEXT="Titularidad">
+                                <node ID="ID_45" POSITION="right" STYLE="fork" TEXT="Vida es el &#250;nico empezador ontol&#243;gico posible porque el Tao nos es inabarcable y el Yang es terciario"/>
+                            </node>
+                            <node ID="ID_16" POSITION="right" STYLE="fork" TEXT="Almismo">
+                                <node ID="ID_17" POSITION="right" STYLE="fork" TEXT="Alma es Yin donde Conciencia es su versi&#243;n Yang, bien lo saben mejor las mujeres. Vanguardia almista inicia guerra "/>
+                            </node>
+                        </node>
+                    </node>
+                </node>
+            </node>
+            <node ID="ID_76" POSITION="right" STYLE="fork" TEXT="Enfocabilidad">
+                <node ID="ID_78" POSITION="right" STYLE="fork" TEXT="Atraemos lo que vibramos o las polaridades de lo que no humildamos integrado"/>
+                <node ID="ID_75" POSITION="right" STYLE="fork" TEXT="Debilidad">
+                    <node ID="ID_77" POSITION="right" STYLE="fork" TEXT="Enfocar la mejora como una sanaci&#243;n o terapia muchas veces cocrea a la enfermedad"/>
+                </node>
+            </node>
+        </node>
+    </node>
+</map>
